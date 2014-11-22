4259fe98789f8188f6049342a8e3ad22bd64c456
Assada <assada@mail.ua>
2014-11-22 04:22:58 +0200

    delete nick from nick tracker list

--- left/tracker/src/main/extensions/src/common/app/options.js
+++ left/tracker/src/main/extensions/src/common/app/options.js
@@ -1,13 +1,52 @@
 console.log('test');
-document.addEventListener('DOMContentLoaded', function () {
-    var addNick = document.getElementById('addNick');
+window.addEventListener('load', function () {
+    var addNick = document.getElementById('addNick'),
+        clearNick = document.getElementById('clearNick'),
+        $btnClearNick = $('#btnClearNickList'),
+        $listOfNick = $('#nickList'),
+        nickList = kango.storage.getItem('nickList') || [];
+    if (nickList.length === 0) {
+        $btnClearNick.attr('disabled', 'disabled');
+        $listOfNick.html('<p class="text-muted">Вы пока не добавили имен для отслеживания</p>');
+    }
+    $.each(nickList, function (id, nick) {
+        $listOfNick.append('<span class="label label-default">' + nick + ' <span class="glyphicon glyphicon-trash delete"></span></span> ');
+    });
     addNick.addEventListener('click', function () {
-        var $nick = $('#nickName').val(),
-            nickList = kango.storage.getItem('nickList') || [];
-        if (!(_.indexOf(nickList, $nick) + 1) && $nick.length > 0) {
-            nickList.push($nick);
+        var $nick = $('#nickName'),
+            $nickVal = $nick.val();
+        if (!(_.indexOf(nickList, $nickVal) + 1) && $nickVal.length > 0) {
+            nickList.push($nickVal);
             kango.storage.setItem('nickList', nickList);
+            $nick.val('');
+            $btnClearNick.removeAttr('disabled');
+            if (nickList.length === 1) {
+                $listOfNick.empty();
+            }
+            $listOfNick.append(' <span class="label label-default">' + $nickVal + ' <span class="glyphicon glyphicon-trash delete"></span></span> ');
         }
         console.log(nickList);
+    });
+    clearNick.addEventListener('click', function () {
+        $btnClearNick.attr('disabled', 'disabled');
+        $listOfNick.empty();
+        kango.storage.removeItem('nickList');
+        nickList = [];
+    });
+    document.querySelector('body').addEventListener('click', function (event) {
+        var $target = $(event.target);
+        if ($target.hasClass('delete')) {
+            var $nick = $target.parent().text().replace(/\s/g, '');
+            $target.parent().remove();
+            if (nickList.indexOf($nick) > -1) {
+                if (nickList.splice(nickList.indexOf($nick), 1)) {
+                    kango.storage.setItem('nickList', nickList);
+                }
+            }
+            if (nickList.length === 0) {
+                $btnClearNick.attr('disabled', 'disabled');
+                $listOfNick.html('<p class="text-muted">Вы пока не добавили имен для отслеживания</p>');
+            }
+        }
     });
 });
