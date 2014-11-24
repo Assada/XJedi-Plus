d09126fd233e9e4e12c9a568b46ac59e6883ea01
Assada <assada@mail.ua>
2014-11-24 03:05:42 +0200

    checking user lvls

--- left/tracker/src/main/extensions/src/common/app/od.js
+++ left/tracker/src/main/extensions/src/common/app/od.js
@@ -21,6 +21,9 @@ define(['app/common', 'jquery.min', 'underscore.min'], function (common) {
                         users = user.split('got'),
                         nick = users[0].trim(),
                         lvl = +(users[1].replace('level ', ''));
+                    if (kango.storage.getItem('checkLvl')) {
+                        main.informLvl(lvl, nick);
+                    }
                     $.each(nicks, function (index, value) {
                         if (value.indexOf(nick) + 1) {
                             if (!(_.indexOf(usedNicks, nick) + 1)) {
@@ -40,17 +43,13 @@ define(['app/common', 'jquery.min', 'underscore.min'], function (common) {
                     var newNick = user.nick,
                         newLvl = user.lvl;
                     $.each(oldList, function (i, oldUser) {
-                        if (oldUser.nick === newNick) {
-                            if (+oldUser.lvl !== +newLvl) {
-                                kango.console.log("СТАРЫЙ НИК: " + oldUser.nick + "ЛВЛ: " + oldUser.lvl + " | НОВЫЙ:" + newNick + "ЛВЛ:" + newLvl);
-                                update = true; //ЛВЛ ИЗМЕНИЛСЯ
-                                kango.console.log('LVL изменился');
-                                kango.ui.notifications.show('Изменение уровня' + newNick, 'Уровень ' + newNick + ((oldUser.lvl > newLvl) ? ' понизился' : ' повысился') + ' до ' + newLvl, 'http://kangoextensions.com/images/logos/kango.png', function () {
-                                    kango.console.log('Notification click');
-                                });
-                            } else {
-                                kango.console.log('LVL !НЕ! изменился');
-                            }
+                        if (oldUser.nick === newNick && +oldUser.lvl !== +newLvl) {
+                            kango.console.log("СТАРЫЙ НИК: " + oldUser.nick + "ЛВЛ: " + oldUser.lvl + " | НОВЫЙ:" + newNick + "ЛВЛ:" + newLvl);
+                            update = true; //ЛВЛ ИЗМЕНИЛСЯ
+                            kango.console.log('LVL изменился');
+                            kango.ui.notifications.show('Изменение уровня' + newNick, 'Уровень ' + newNick + ((oldUser.lvl > newLvl) ? ' понизился' : ' повысился') + ' до ' + newLvl, 'http://kangoextensions.com/images/logos/kango.png', function () {
+                                kango.console.log('Notification click');
+                            });
                         }
                     });
                 });
@@ -58,6 +57,27 @@ define(['app/common', 'jquery.min', 'underscore.min'], function (common) {
                 update = true;
             }
             return update;
+        },
+        getLvl : function (url) {
+            console.log('CHECKING');
+            var html = common.getPageHtml(url),
+                content = $($.parseHTML(html)).find('.profile-property-row>span[style="color: #88eeff; "]'),
+                lvl = +$(content[0]).text();
+            kango.storage.setItem('profileLvl', lvl);
+            //console.log();
+        },
+        informLvl : function (lvl, nick) {
+            var mLvl = kango.storage.getItem('profileLvl'),
+                lvlNicks = kango.storage.getItem('lvlNicks') || {},
+                lvlNick = lvlNicks[nick] || {};
+            //console.log(lvlNicks.nick + 'GET: ' + lvlNicks.lvl + ' LVL!');
+            if (+lvl === +mLvl && +lvlNick.lvl !== +lvl) {
+                kango.ui.notifications.show(nick + ' достиг ' + lvl + ' уровня!', 'Уровень ' + nick + ' стал приемлемым для вас!', 'http://kangoextensions.com/images/logos/kango.png', function () {
+                    kango.console.log('Notification click');
+                });
+                lvlNicks[nick] = {lvl : +lvl};
+                kango.storage.setItem('lvlNicks', lvlNicks);
+            }
         }
     };
     return main;
