32ec4c6155483f4f732db68aae168014a13ab9b2
Assada <assada@mail.ua>
2014-11-26 00:51:09 +0200

    getting server data and save to storage

--- left/tracker/src/main/extensions/src/common/app.js
+++ left/tracker/src/main/extensions/src/common/app.js
@@ -6,15 +6,19 @@ requirejs.config({
 });
 kango.console.log('App loaded');
 require(
-    ['app/od'],
-    function (od) {
-        (function run() {
+    ['app/od', 'app/serversData.js'],
+    function (od, server) {
+        (function profile() {
             od.init();
             if (kango.storage.getItem('checkLvl')) {
                 od.getLvl(kango.storage.getItem('profileLink'));
             }
             console.log(kango.storage.getItem('profileLvl') || false);
-            setTimeout(run, 30000);
+            setTimeout(profile, 30 * 1000);
+        }());
+        (function serverData() {
+            server.init();
+            setTimeout(serverData, 10 * 1000);
         }());
     }
 );
---
+++ left/tracker/src/main/extensions/src/common/app/serversData.js
@@ -0,0 +1,58 @@
+/**
+ * Created by Alex on 25.11.2014.
+ */
+define(['app/common', 'jquery.min'], function (common) {
+    var main = {
+        init: function () {
+            var html = common.getPageHtml('http://xjedi.com/monitor/monitor.jsp'),
+                test = $.parseHTML(html);
+            kango.storage.setItem('serversData', main.parser(test));
+            kango.console.log(main.parser(test));
+        },
+        parser: function (content) {
+            var allServers = [];
+            $(content).each(function () {
+                kango.console.log('parser start');
+                var playersInServer = [],
+                    $this = $(this),
+                    server_names = ($this.find('.header1').text()).split(/\n/);
+                if (server_names[1]) {
+                    var name = server_names[1].replace(' ', '').trim().replace(/XJedi |UA |- /gi, ''),
+                        players = $this.find('.header2[style!="padding-left:4px;"]').text().replace('                   ', '').split(/\n/)[2].trim().split('/'),
+                        currPlayers = +players[0].replace('Гравців ', ''),
+                        maxPlayers = +players[1],
+                        map = $this.find('.header2[style="padding-left:4px;"]').text(),
+                        mapSrc = $this.find('img').attr('src'),
+                        player_list = $this.find('div[style="width:200px; padding:8px 0px 0px 15px; float:left; left:10px;"]'),
+                        scores = ($this.find('div[style="width:30px; height:14px;border-bottom:1px solid #C0C6CC; overflow: hidden; float:left; "]').text()).split(/\n/);
+                    $(player_list).each(function () {
+                        var $thisP = $(this),
+                            users = $thisP.find('div[style*="width:140px; height:14px; border-bottom:1px solid #C0C6CC;"]'),
+                            i = 1;
+                        $(users).each(function () {
+                            var user = $(this).html().replace('                    ', '').trim();
+                            if (i !== 1) {
+                                i++;
+                            }
+                            if (user.replace('                ', '').length !== 0) {
+                                playersInServer.push({ user: user, score: +scores[i]});
+                            }
+                            i++;
+                        });
+                    });
+                    allServers.push({
+                        name : name,
+                        currPlayers : currPlayers,
+                        maxPlayers : maxPlayers,
+                        map: map,
+                        mapSrc: mapSrc,
+                        players: playersInServer
+                    });
+                }
+            });
+            kango.console.info('parser loaded');
+            return allServers;
+        }
+    };
+    return main;
+});
