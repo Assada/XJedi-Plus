4ef5141eee4efb55dcad0050e862f2df40b67b71
Assada <assada@mail.ua>
2014-11-27 05:00:59 +0200

    fix 3 player activity and new function "getName"

--- left/tracker/src/main/extensions/src/common/app/common.js
+++ left/tracker/src/main/extensions/src/common/app/common.js
@@ -20,6 +20,30 @@ define(['jquery.min'], function () {
                 }
             });
             return test;
+        },
+        getName: function (name) {
+            var namee = name.match(/[a-zA-Z\s-]+/g),
+                lengthOld = 0;
+            if (namee) {
+                if (namee[0].trim() !== 'Guest' && namee[0].trim() !== '...') {
+                    lengthOld = namee.length;
+                    if (lengthOld  >= 4) {
+                        namee.pop();
+                        namee.shift();
+                    }
+                    if (lengthOld >= 2) {
+                        namee.shift();
+                    }
+                    if (namee[0] === namee[0].toUpperCase()) {
+                        namee.shift();
+                    }
+                    return namee[0].trim();
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
         }
     };
 });
