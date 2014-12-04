51ccf95a9fad5fa2016de0c3eecd02410fe3f17b
Assada <assada@mail.ua>
2014-12-04 23:03:41 +0200

    new design for popup

---
+++ left/tracker/src/main/extensions/src/common/app/popup.js
@@ -0,0 +1,8 @@
+/*
+var serverInfo = kango.storage.getItem('playersActivity') || [],
+    tabs = $('.nav-tabs'),
+    content = $('.tab-content');
+$(serverInfo).each(function () {
+
+});*/
+$('.selectpicker').selectpicker();
