4eb0f70cbb2c9b024e6180c0bfba1d92e5bef13d
Assada <assada@mail.ua>
2014-12-11 18:08:01 +0200

    test

--- left/tracker/src/main/extensions/src/common/app/popup.js
+++ left/tracker/src/main/extensions/src/common/app/popup.js
@@ -1,24 +1,25 @@
-KangoAPI.onReady(function() {
+KangoAPI.onReady(function () {
     function loadd() {
         var test = _.template($('#template').text()),
             server = kango.storage.getItem('popUpLastServerOpenId') || 0;
         if ($('.monitor').html(test({'servers' : kango.storage.getItem('serversData')}))) {
-            $('.selectpicker').selectpicker();
             $('#server-' + server).show();
-            $('.selectpicker').selectpicker('val', server);
         }
-        $('select').on('change', function (e) {
-            var $optionSelected = $("option:selected", this),
-                valueSelected = this.value,
-                $serverId = +$(this).val();
+        $('select').on('change', function () {
+            var $serverId = +$(this).val();
             if ($('.tab-content').hide()) {
                 $('#server-' + $serverId).show();
                 kango.storage.setItem('popUpLastServerOpenId', +$serverId);
             }
         });
+        return true;
     }
     loadd();
     setInterval(function () {
-        loadd();
+        if (loadd()) {
+            var $selectpicker = $('.selectpicker');
+            $selectpicker.selectpicker();
+            $selectpicker.selectpicker('val', server);
+        }
     }, 10 * 1000);
 });
